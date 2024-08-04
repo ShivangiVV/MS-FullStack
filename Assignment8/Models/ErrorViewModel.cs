db927b21eb58fab951f83620dc112fc1d96d16b9
ShivangiVV <85619644+ShivangiVV@users.noreply.github.com>
2024-08-04 13:49:52 +0530

    Delete Assignment8 directory

--- Assignment8/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Assignment8.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
